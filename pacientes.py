7162919393305db2467e8443e94451b47fec7773
HipolitoGimenez <hipolitogimenez970@gmail.com>
2025-04-28 14:56:46 -0300

    primer commit

---
+++ pacientes.py
@@ -0,0 +1,17 @@
+class pacientes:
+
+ def __init__(self, nombre, DNI, fecha_de_nacimiento, sexo, telefono, tipo_de_sangre, centro_de_salud):
+    
+        self.nombre=nombre
+
+        self.DNI=DNI
+        self.fecha_de_nacimiento=fecha_de_nacimiento
+        self.sexo=sexo
+        self.telefono=telefono
+        self.tipo_de_sangre=tipo_de_sangre
+        self.centro_de_salud=centro_de_salud
+
+
+    
+    
+    
