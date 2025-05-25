533cea267f7280ad12a59d4d06672d0f8332eca8
HipolitoGimenez <hipolitogimenez970@gmail.com>
2025-05-25 16:34:09 -0300

    Algunos arreglos

--- main.py.py
+++ main.py.py
@@ -1,7 +1,7 @@
- 
+import datetime
+import random
 from donantes import *
 from centro_de_salud import *
-from datetime import datetime
 from organo import Organo
 from cirujano import Cirujano
 from receptor import Receptor
@@ -15,19 +15,20 @@ from INCUCAI import INCUCAI
 
 
 #Cirujanos
-cirujano1 = Cirujano( "Adrian","cardiovascular",True,0,"12345678","1980-01-01","masculino","1122334455","A+","Hospital Italiano", "[Corazon]")
-cirujano2 = Cirujano( "Juan","Pulmonar",True,0,"48545678","1975-06-11","masculino","114434455","B+","Hospital Aleman", "[Pulmones]")
-
-
+cirujano1 = Cirujano( "Adrian","cardiovascular",True,0,"12345678", datetime.strptime("01-11-1980", "%d-%m-%Y"),"masculino","1122334455","A+","Hospital Italiano", "[Corazon]")
+cirujano2 = Cirujano( "Juan","Pulmonar",True,0,"48545678", datetime.strptime("11-06-1975", "%d-%m-%Y"),"masculino","114434455","B+","Hospital Aleman", "[Pulmones]")
+cirujano3 = Cirujano("Carla", "Hepática", True, 0, "56789456", datetime.strptime("22-02-1982", "%d-%m-%Y"), "femenino", "1166778899", "O+", "Hospital Provincial de Córdoba", "[Hígado]")
+cirujano4 = Cirujano("Esteban", "Renal", True, 0, "47859632", datetime.strptime("03-12-1978", "%d-%m-%Y"), "masculino", "1155998877", "AB-", "Hospital José María Cullen", "[Riñón]")
 
 #Personas/ donantes
-persona1 = Persona("Adriana", "23060432", "02-06-1978", "F", "2494534523", "A+", "Italiano")
-persona2 = Persona("Juan", "25258456", "12-01-1994", "M", "15252523", "B+", "Aleman")
+#donante1 = Donante("Adriana","23060432",fecha_de_nacimiento=datetime.strptime("02-06-1978", "%d-%m-%Y"),"F","2494534523","A+","hospital_italiano")
+#donante2 = Donante("Juan","25258456",fecha_de_nacimiento=datetime.strptime("12-01-1994", "%d-%m-%Y"),"M","15252523", "B+", "hospital_aleman", "Pulmón")
 
 #Receprores
-receptor1 = Receptor("Lucía","2345768988","04-11-2000","F","1122334455","A+","Hospital Italiano","Corazón","Estable","2025-05-05", "Alta","Paro cardíaco")
-receptor2= Receptor("Claudio","33545966","09-06-1999","M","1155566685","B+","Hospital Aleman","Pulmon","Estable","2025-05-05", "Alta","Neumonia")
-
+receptor1 = Receptor("Lucía","2345768988",datetime.strptime("04-11-2000", "%d-%m-%Y"),"F","1122334455","A+","Hospital Italiano","Corazón","Estable","2025-05-05", "Alta","Paro cardíaco")
+receptor2= Receptor("Claudio","33545966", datetime.strptime("09-06-1999", "%d-%m-%Y"),"M","1155566685","B+","Hospital Aleman","Pulmon","Estable","2025-05-05", "Alta","Neumonia")
+receptor3 = Receptor("María", "40234567", datetime.strptime("15-03-1985", "%d-%m-%Y"), "F", "1133445566", "O+", "Hospital Provincial de Córdoba", "Hígado", "Crítico", "2025-05-20", "Alta", "Cirrosis hepática")
+receptor4 = Receptor("Jorge", "38999888", datetime.strptime("27-08-1972", "%d-%m-%Y"), "M", "1177889900", "AB-", "Hospital José María Cullen", "Riñón", "Estable", "2025-05-22", "Alta", "Insuficiencia renal")
 
 #Organos
 organo1 = Organo("Corazon")  
@@ -38,6 +39,8 @@ organo2.fecha_ablacion(datetime.now())
 #Centros de salud
 hospital1=CentroDeSalud("Hospital Italiano", "AV Italia 145", "CABA", "Buenos Aires", "84515262385")
 hospital2=CentroDeSalud("Hospital Aleman", "AV Italia 145", "CABA", "Buenos Aires", "48545262385")
+hospital3 = CentroDeSalud("Hospital Provincial de Córdoba", "Av. Vélez Sarsfield 1200", "Córdoba Capital", "Córdoba", "3514896523")
+hospital4 = CentroDeSalud("Hospital José María Cullen", "Av. Freyre 2150", "Santa Fe", "Santa Fe", "3424578912")
 
 #Agrego los vehiculos
 ambulancia1=Auto(120)
@@ -72,6 +75,17 @@ print(cirujano1.operacion())
 print(cirujano1) 
 print("Se encuentra disponible {cirujano1.}")
 print(organo1)##Corre
+#lista_donantes = [donante1, donante2]
+lista_receptores = [receptor1, receptor2, receptor3, receptor4]
+
+#print("=== LISTADO DE DONANTES ===")
+#for d in lista_donantes:
+#    print(d)
+lista_receptores = [receptor1, receptor2, receptor3, receptor4]
+
+print("=== LISTADO DE RECEPTORES ===")
+for receptor in lista_receptores:
+    print(receptor)
 
 
 cirujano_asignado = hospital1.asignar_cirujano(receptor1.organo_necesario)
